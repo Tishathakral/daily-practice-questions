8bc8bbf0e3a845f0a05332ef98abe30cea1214aa
Tisha Thakral <99125026+Tishathakral@users.noreply.github.com>
2025-02-11 20:32:41 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -5,9 +5,9 @@ public:
         int boy;
         for(boy=0;boy<zeta.size();boy++){
             if(boy != zeta[boy]){
-                break;
+                return boy;
             }
         }
-        return boy;
+        return zeta.size();
     }
 };
